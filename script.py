a134aed494791734f1ac95d68855b488916b222c
LyudmilaTretyakova <lyudmila_1997@list.ru>
2022-11-30 19:36:00 +0300

    try1

--- script.py
+++ script.py
@@ -6,13 +6,13 @@ import aiohttp
 import asyncio
 from requests_futures.sessions import FuturesSession
 import tree
+import json
 
 trees = []
 
 valid_regex = r"^https?\:\/\/([\w\.]+)wikipedia.org\/wiki\/([\w]+\_?)+"
 links = dict()
-global checkset
-checkset = set()
+
 loop = asyncio.get_event_loop()
 def check_valid(link):
     if re.split(valid_regex, link):
@@ -27,7 +27,7 @@ def check_valid_int(intvalue):
     if intvalue>=1 and intvalue<=20:
         return True
 
-
+""" its my bad tryis without good asyncio
 async def run_link(link):
     try:
         response = await requests.get(link)
@@ -77,26 +77,41 @@ async def main(n,link):
 
     print(links)
 
+async def check_valida(session,link):
+    async with session.get(link) as resp:
+        if re.split(valid_regex, link):
+            if resp.status_code != 200:
+                return False
+        else:
+            return False
+"""
 async def run_linkas(session,link):
     async with session.get(link) as resp:
         response = await resp.text()
         links = []
         soup = BeautifulSoup(response, "html.parser")
-        links_ = soup.find(id="bodyContent").find_all("a")
-        for link in links_:
-            if 'href' in link.attrs:
-                if link.attrs['href'].startswith('/wiki/'): #and check_valid("https://en.wikipedia.org" + link['href']):
-                    links.append("https://en.wikipedia.org" + link['href'])
-                elif link.attrs['href'].startswith('https'): #and check_valid(link['href']): #DO IT WITH WIKIIIIIIIII
-                    links.append(link['href'])
+
+
+        links_ = soup.find(id="bodyContent").find_all('a', href=True)
+
+        for link1 in links_:
+            if 'href' in link1.attrs:
+                if link1.attrs['href'].startswith('/wiki/'):#and check_valida(session,"https://en.wikipedia.org" + link['href']):
+                    links.append("https://en.wikipedia.org" + link1['href'])
+                elif link1.attrs['href'].startswith('https://en.wikipedia') :#and check_valida(session,link['href']): #DO IT WITH WIKIIIIIIIII
+                    links.append(link1['href'])
         return link,links
 
 
 async def mains(url, n):
     checksett = set()
+    checkset = set()
+    trees = []
+    allofall = {}
     async with aiohttp.ClientSession() as session:
         pokemon_url = [url]
         checksett.add(url)
+        allofall[url] = [url]
         for i in range(0,n):
             tasks=[]
             for j in pokemon_url:
@@ -104,28 +119,35 @@ async def mains(url, n):
                     tasks.append(asyncio.ensure_future(run_linkas(session, j)))
 
             original_pokemon = await asyncio.gather(*tasks)
-            print(original_pokemon)
-            checkset |= checksett
-            listtree = []
-            mytree = tree.Tree(original_pokemon[0][0])
-            for pokemon in original_pokemon[0]:
-                mytreech = tree.Tree(original_pokemon[0][1])
-                mytreech.left(original_pokemon[0][0])
-                listtree.append(mytreech)
-                pokemon_url.append(pokemon[1])
-            mytree.right(listtree)
 
-            checksett |= set(original_pokemon[0])
+            checkset |= checksett
+
+            for pokemon in original_pokemon:
+                pokemon_url =pokemon[1]
+                #trees += pokemon[1]
+                checksett |= set(pokemon[1])
+                allofall[pokemon[0]] = pokemon_url
+    # for x in allofall: \\ its to print
+    #     print(x)
+    #     print(x, ':', allofall[x])
+    return allofall
+
+def savejson(namefile, data):
+    with open(namefile, 'w+') as fp:
+        json.dump(data, fp)
+
 
 
 if __name__ == "__main__":
+    global checkset
+    checkset = set()
     input_link = input("Input Wiki link: ")
     if check_valid(input_link):
         print("Url is not valid, please enter valid url")
     else:
         input_int = int(input("Input Integer link: "))
         if check_valid_int(input_int):
-            #asyncio.run(main(input_int,input_link))
-            asyncio.run(mains(input_link,input_int))
+            data = asyncio.run(mains(input_link,input_int))
+            savejson(r"D:\testtask\testtask\data.json",data)
         else:
             print("Int is not valid, please enter valid int from 1 to 20")
